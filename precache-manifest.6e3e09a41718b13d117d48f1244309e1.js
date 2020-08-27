8c25a21fadeb8f5dfc10fdb338b4e4b4b7f360e7
ango-books <satenikmargaryanm@gmail.com>
2020-08-27 10:38:23 +0400

    Updates

---
+++ precache-manifest.6e3e09a41718b13d117d48f1244309e1.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96cf0f77fff165a549dc58a0e8fed64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f69d65a5ce651fbc41",
+    "url": "/static/css/main.7d87f663.chunk.css"
+  },
+  {
+    "revision": "410254ef3f2b06a848ac",
+    "url": "/static/js/2.2eacfb9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2eacfb9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f69d65a5ce651fbc41",
+    "url": "/static/js/main.4c847d66.chunk.js"
+  },
+  {
+    "revision": "c617e9e0c3affd445c9f",
+    "url": "/static/js/runtime-main.ba2dcd13.js"
+  },
+  {
+    "revision": "3d7caca9df969de2cd3173feb2fa31a0",
+    "url": "/static/media/Andranik_Khachatryan.3d7caca9.jpg"
+  },
+  {
+    "revision": "0604f22a02065c9661ad11460f8f53f5",
+    "url": "/static/media/Davit_Kocharian.0604f22a.png"
+  },
+  {
+    "revision": "026bec54582e2f6140f08184de2bd472",
+    "url": "/static/media/Erik_Harutyunyan.026bec54.jpg"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/static/media/Futura.f9f02ed0.ttf"
+  },
+  {
+    "revision": "cbf10de30bcd88d058e5093c5938933c",
+    "url": "/static/media/FuturaBoldUnicode.cbf10de3.ttf"
+  },
+  {
+    "revision": "e2491e4368c5e698504624757588b884",
+    "url": "/static/media/Hrant_Khachatrian.e2491e43.jpg"
+  },
+  {
+    "revision": "5f5dd5d8167e63b3701ec4c2ab46e6d5",
+    "url": "/static/media/Hrayr_Harutyunyan.5f5dd5d8.png"
+  },
+  {
+    "revision": "86b3f4a34144315967a454fae00267ba",
+    "url": "/static/media/Levan_Tsinadze.86b3f4a3.png"
+  },
+  {
+    "revision": "05f0bd561963a84aae2428ad76dfc510",
+    "url": "/static/media/Manvel_Avetissian.05f0bd56.jpeg"
+  },
+  {
+    "revision": "10798e8d4b8c9c36757251737cf2e4f1",
+    "url": "/static/media/Robert_Yenokyan.10798e8d.jpg"
+  },
+  {
+    "revision": "6ed50e2697c1fa009250c7955ef3d0ab",
+    "url": "/static/media/Shant_Navasardyan.6ed50e26.jpg"
+  },
+  {
+    "revision": "e4a9ad6f800c352500a533570c473e74",
+    "url": "/static/media/Stepan_Sargsyan.e4a9ad6f.jpg"
+  },
+  {
+    "revision": "490f1935d64a35fd8d99ec4e2fb1b002",
+    "url": "/static/media/aboutFrame.490f1935.svg"
+  },
+  {
+    "revision": "0c60ba709479f42278ba1a4fe8c161a9",
+    "url": "/static/media/agendaFrame.0c60ba70.png"
+  },
+  {
+    "revision": "746e3bfa599c28a234770074652c8609",
+    "url": "/static/media/arrowDown.746e3bfa.svg"
+  },
+  {
+    "revision": "57709541b314101333cadde98928e5e4",
+    "url": "/static/media/festPicAbout.57709541.svg"
+  },
+  {
+    "revision": "780f414996b26e8f30172b8c6284d1ce",
+    "url": "/static/media/footerLogo.780f4149.svg"
+  },
+  {
+    "revision": "cb80c53741c565b43d6602e29d9f083d",
+    "url": "/static/media/footerVector.cb80c537.svg"
+  },
+  {
+    "revision": "abcb751f153d092d9e75f63a2f15d617",
+    "url": "/static/media/homeFrame.abcb751f.svg"
+  },
+  {
+    "revision": "db17f351220c55cc990ad6fd858674ac",
+    "url": "/static/media/logo.db17f351.svg"
+  },
+  {
+    "revision": "0a85bc281309d921e0381bbbe24f9f87",
+    "url": "/static/media/number_1.0a85bc28.svg"
+  },
+  {
+    "revision": "fb97a5f504d947778ed17db3b08b5ead",
+    "url": "/static/media/number_2.fb97a5f5.svg"
+  },
+  {
+    "revision": "7563d3f56d015083ce1f0934d60a55ce",
+    "url": "/static/media/number_3.7563d3f5.svg"
+  },
+  {
+    "revision": "40f14d643b3dfa98e0ee0fc750a2a956",
+    "url": "/static/media/pinkArrowDown.40f14d64.svg"
+  },
+  {
+    "revision": "750c614bebd97d4609b8135fc10dd402",
+    "url": "/static/media/speakerFrame.750c614b.svg"
+  },
+  {
+    "revision": "2392a4729674320124fd516b9aba3e37",
+    "url": "/static/media/sponsorFrame.2392a472.svg"
+  },
+  {
+    "revision": "b716993d1923e69b9ad4782cd3b40ef3",
+    "url": "/static/media/sponsor_vector_1.b716993d.svg"
+  },
+  {
+    "revision": "4eecc1b079cd66f8df32275a9dd47a46",
+    "url": "/static/media/sponsor_vector_3.4eecc1b0.svg"
+  },
+  {
+    "revision": "41661815818da4f25241f8d6832b31c8",
+    "url": "/static/media/sponsor_vector_4.41661815.svg"
+  },
+  {
+    "revision": "05ea60c5d52bf8cb80b2405798dd74ef",
+    "url": "/static/media/sponsorsBackground.05ea60c5.svg"
+  },
+  {
+    "revision": "1b39b6f25d8715f96317823ffe1c6a71",
+    "url": "/static/media/topicVector_1.1b39b6f2.svg"
+  },
+  {
+    "revision": "559865705b4da2e5d22335cded8fb4b2",
+    "url": "/static/media/topicVector_2.55986570.svg"
+  },
+  {
+    "revision": "04ef42c44b06f61a0379e18608bed30f",
+    "url": "/static/media/topicVector_3.04ef42c4.svg"
+  },
+  {
+    "revision": "9701a4f5db3d0cbc9974e10b0ec64e6b",
+    "url": "/static/media/topicVector_4.9701a4f5.svg"
+  },
+  {
+    "revision": "74159c1a41ec675e5f636a397cf84237",
+    "url": "/static/media/topicVector_5.74159c1a.svg"
+  }
+]);
