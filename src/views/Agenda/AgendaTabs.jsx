03a9c3f501d69c08a44255d5999f77f34fa3da4a
satenikmargaryan <satenikmargaryanm@gmail.com>
2022-08-08 00:20:41 +0400

    Added new agenda table

---
+++ src/views/Agenda/AgendaTabs.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import Paper from "@mui/material/Paper";
+import {
+  ViewState,
+  GroupingState,
+  IntegratedGrouping,
+} from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  Resources,
+  Appointments,
+  GroupingPanel,
+  DayView,
+} from "@devexpress/dx-react-scheduler-material-ui";
+
+import CustomAppointment from "./CustomAppointment";
+
+const Tabs = ({ days, locations }) => {
+  const [activeIndex, setActiveIndex] = useState("1");
+  const handleClick = (index) => setActiveIndex(index);
+  const checkActive = (index, className) =>
+    activeIndex === index ? className : "";
+
+  const selectedDay = days.filter((day) => day.dateIndex === activeIndex);
+
+  const appointments = selectedDay[0][activeIndex].map((speakerInfo) => {
+    const filteredSpeakerInfo = {
+      id: 0,
+      speaker: speakerInfo.speaker,
+      title: speakerInfo.title,
+      roomId: speakerInfo.roomId,
+      startDate: new Date(
+        selectedDay[0].year,
+        selectedDay[0].month,
+        selectedDay[0].day,
+        speakerInfo.startHour,
+        speakerInfo.startMinutes
+      ),
+      endDate: new Date(
+        selectedDay[0].year,
+        selectedDay[0].month,
+        selectedDay[0].day,
+        speakerInfo.endHour,
+        speakerInfo.endMinutes
+      ),
+    };
+    return filteredSpeakerInfo;
+  });
+
+  const schedulerData = {
+    data: appointments,
+    resources: [
+      {
+        fieldName: "roomId",
+        title: "Location",
+        instances: locations,
+      },
+    ],
+    grouping: [
+      {
+        resourceName: "roomId",
+      },
+    ],
+  };
+
+  const currentDate = `${selectedDay[0].year}-${selectedDay[0].month + 1}-${selectedDay[0].day}`;
+
+  return (
+    <>
+      <div className="tabs">
+        {days.map((day, index) => {
+          return (
+            <button
+              key={index}
+              className={`tab ${checkActive(day.dateIndex, "active")} date`}
+              onClick={() => handleClick(day.dateIndex)}
+            >
+              {day.date}
+            </button>
+          );
+        })}
+      </div>
+      <div className="panels">
+        <Paper>
+          <Scheduler data={schedulerData.data} height={660}>
+            <ViewState currentDate={currentDate} />
+            <GroupingState grouping={schedulerData.grouping} />
+
+            <DayView startDayHour={9} endDayHour={15} />
+            <Appointments appointmentComponent={CustomAppointment} />
+            <Resources
+              data={schedulerData.resources}
+              mainResourceName="roomId"
+            />
+
+            <IntegratedGrouping />
+            <GroupingPanel />
+          </Scheduler>
+        </Paper>
+      </div>
+      <div className="cards">
+        {appointments.map((appointment, agendaIndex) => {
+          return (
+            <div key={agendaIndex}>
+              <CustomAppointment data={appointment} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
---
+++ src/views/Agenda/CustomAppointment.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { differenceInMinutes } from "date-fns";
+import { useTranslation } from "react-i18next";
+
+import { Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+
+const CustomAppointment = ({ style, ...restProps }) => {
+  const { t } = useTranslation("common");
+  const locations = t("agenda.locations", { returnObjects: true });
+
+  function formatAMPM(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    const strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+  }
+
+  function timeConvert(n) {
+    const num = n;
+    const hours = num / 60;
+    const rhours = Math.floor(hours);
+    const minutes = (hours - rhours) * 60;
+    const rminutes = Math.round(minutes);
+    return `${
+      rhours
+        ? `${rhours} ${t("agenda.hourText", { returnObjects: true })} ${
+            rhours > 1 ? `s` : ""
+          }`
+        : ""
+    } ${
+      rminutes
+        ? `${rminutes} ${t("agenda.minuteText", {
+            returnObjects: true,
+          })}`
+        : ""
+    }`;
+  }
+
+  function getCurrentRoom(roomId) {
+    return locations.filter((location) => location.id === roomId)[0];
+  }
+
+  const mins = differenceInMinutes(
+    restProps.data.endDate,
+    restProps.data.startDate
+  );
+
+  if (!restProps) {
+    return false;
+  }
+  return (
+    <Appointments.AppointmentContent {...restProps}>
+      <div className="agenda-container">
+        <div className="time">
+          <div className="time-preview">
+            <span className="ampm">{formatAMPM(restProps.data.startDate)}</span>
+            <br></br>
+            <span className="time-diffs">{timeConvert(mins)}</span>
+          </div>
+          <div className="course-info">
+            <div className="progress-container">
+              <span className="speaker-text">{restProps.data.speaker}</span>
+              <br></br>
+              <span className="progress-text">{restProps.data.title}</span>
+            </div>
+          </div>
+        </div>
+        <span className="room">{getCurrentRoom(restProps.data.roomId).text}</span>
+      </div>
+    </Appointments.AppointmentContent>
+  );
+};
+
+export default CustomAppointment;
