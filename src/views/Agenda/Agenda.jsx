ff8dccb7035b859ed94454ef2be89d45441dc7a6
satenikmargaryan <satenikmargaryanm@gmail.com>
2023-08-30 21:12:31 +0400

    Fixed agenda removed pretalx.

--- src/views/Agenda/Agenda.jsx
+++ src/views/Agenda/Agenda.jsx
@@ -21,7 +21,7 @@ const Agenda = () => {
   const pathArray = window.location.pathname.split("/");
   const year = pathArray[1];
 
-  if (year == 2022) {
+  if (year >= 2022) {
     return (
       <>
         <div className="container">
@@ -72,69 +72,6 @@ const Agenda = () => {
           </div>
         </div>
         <Tabs days={days} locations={locations}></Tabs>
-        {t("agenda.bottomText") && (
-          <div className="small-text">
-            <p>{t("agenda.bottomText")}</p>
-          </div>
-        )}
-        <div className="space-100-height"></div>
-        <div className="space-85-height"></div>
-      </>
-    );
-  }
-
-  if (year >= 2023) {
-    return (
-      <>
-        <div className="container">
-          <div className="agenda">
-            <div className="space-100-height"></div>
-            <div className="space-85-height"></div>
-            <div className="flex flex-space-between column">
-              <div></div>
-              <div className="flex flex-space-between full-width first-section">
-                <div className="flex align-start column">
-                  <div className="space-35-height"></div>
-                  <div className="big-text">
-                    <div
-                      dangerouslySetInnerHTML={{ __html: t("agenda.title") }}
-                    ></div>
-                  </div>
-                  <p className="small-text">
-                    <span>{t("agenda.yerevanTime")}</span>
-                  </p>
-                </div>
-
-                <img src={agendaFrame} className="main" alt="" />
-              </div>
-            </div>
-
-            <div className="space-85-height"></div>
-
-            <p className="paragraph">{t("agenda.text")}</p>
-            <div className="space-100-height"></div>
-            <div className="flex center">
-              <div className="btn">
-                <div className="btn-cover white-btn-cover big-btn-cover"></div>
-                <button
-                  className="big bordered big-white-border"
-                  onClick={scrollDown}
-                >
-                  <span className="uppercase">{t("agenda.btnText")}</span>
-                </button>
-              </div>
-            </div>
-
-            <div className="space-100-height"></div>
-            {t("agenda.aboveText") && (
-              <div className="small-text">
-                <p>{t("agenda.aboveText")}</p>
-              </div>
-            )}
-          </div>
-        </div>
-        <iframe className="pretalx" style={{'border-width': 0}} width="100%" height="800" src="https://pretalx.herokuapp.com/datafest-2023/schedule" title=""></iframe>
-
         {t("agenda.bottomText") && (
           <div className="small-text">
             <p>{t("agenda.bottomText")}</p>
