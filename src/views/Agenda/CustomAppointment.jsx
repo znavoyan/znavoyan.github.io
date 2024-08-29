f5796cc6ba19a2b3ec79512ffa999078ff7d802c
satenikmargaryan <satenikmargaryanm@gmail.com>
2024-08-29 16:23:38 +0400

    Fixed the sequence of the collaborators on the card.

--- src/views/Agenda/CustomAppointment.jsx
+++ src/views/Agenda/CustomAppointment.jsx
@@ -116,7 +116,7 @@ const CustomAppointment = ({ style, ...restProps }) => {
             <div className="space-10-height"></div>
           </div>
         )}
-        {collaborators && (
+        {collaborators.length && (
           <div className="flex column">
             <div className="collaborators">
               {collaborators.map((collaborator) => {
